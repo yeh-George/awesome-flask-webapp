c7767699ef70a5af9ed0ab624d6f7a36a7e91329
yeh-George <ycj012@163.com>
2020-03-23 19:19:45 +0800

    make models

---
+++ awesome_flask_webapp/models.py
@@ -0,0 +1,30 @@
+from awesome_flask_webapp.extensions import db
+
+
+class Permission(db.Model):
+    pass
+
+
+class Role(db.Model):
+    pass
+
+
+class User(db.Model):
+    pass
+
+
+class Tag(db.Model):
+    pass
+
+
+class Blog(db.Model):
+    pass
+
+
+class Comment(db.Model):
+    pass
+
+
+class Collect(db.Model):
+    pass
+
