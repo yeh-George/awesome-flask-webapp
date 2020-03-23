f29a9a2eedf67b5028558057995949e97f36940d
yeh-George <ycj012@163.com>
2020-03-23 19:19:02 +0800

    app factory

--- awesome_flask_webapp/__init__.py
+++ awesome_flask_webapp/__init__.py
@@ -1,15 +1,55 @@
 import os
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 from awesome_flask_webapp.settings import config
+from awesome_flask_webapp.extensions import db
 
-app = Flask('awesome_flask_webapp')
-config_name = os.getenv('FLASK_CONFIG', 'developement')
-app.config.from_object(config[config_name])
 
-@app.route('/')
-def index():
-    return '<h1>hello, awesome_flask_webapp</h1>'
+def create_app(config_name=None):
+    if config_name is None:
+        config_name = os.getenv('FLASK_CONFIG', 'development')
+
+    app = Flask('awesome_flask_webapp')
+    app.config.from_object(config[config_name])
+
+    # 扩展初始化
+    register_extensions(app)
+    # 注册蓝本
+    register_blueprints(app)
+    # 注册错误处理函数
+    register_errorhandlers(app)
+    # 注册模板上下文处理函数
+    register_template_context(app)
+    # 注册shell上下文处理函数
+    register_shell_context(app)
+    # 注册自定义shell命令
+    register_commands(app)
+
+    return app
+
+
+def register_extensions(app):
+    db.init_app(app)
+
+
+def register_blueprints(app):
+    pass
+
+
+def register_errorhandlers(app):
+    pass
+
+
+def register_shell_context(app):
+    pass
+
+
+def register_template_context(app):
+    pass
+
+
+def register_commands(app):
+    pass
+
 
