43e21be1f58fd5fc2498fa93547d6e14e1394db6
yeh-George <ycj012@163.com>
2020-03-24 10:49:08 +0800

    models.py: basic User

--- awesome_flask_webapp/models.py
+++ awesome_flask_webapp/models.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from awesome_flask_webapp.extensions import db
 
 
@@ -10,14 +14,29 @@ class Role(db.Model):
 
 
 class User(db.Model):
-    pass
+    # account info
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, index=True)
+    email = db.Column(db.String(254), index=True)
+    password_hash = db.Column(db.String(128))
+    # user info
+    name = db.Column(db.String(30))
+    bio = db.Column(db.String(255))
+    location = db.Column(db.String(50))
+    memeber_since = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Tag(db.Model):
     pass
 
 
-class Blog(db.Model):
+class Post(db.Model):
     pass
 
 
