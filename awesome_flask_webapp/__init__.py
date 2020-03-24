e575846e67d1cf47e35946cbd557babc5df16598
yeh-George <ycj012@163.com>
2020-03-24 10:26:58 +0800

    main/index.html for testing

--- awesome_flask_webapp/__init__.py
+++ awesome_flask_webapp/__init__.py
@@ -3,8 +3,8 @@ import os
 from flask import Flask
 
 from awesome_flask_webapp.settings import config
-from awesome_flask_webapp.extensions import db
-
+from awesome_flask_webapp.extensions import db, bootstrap
+from awesome_flask_webapp.blueprints.main import main_bp
 
 def create_app(config_name=None):
     if config_name is None:
@@ -31,10 +31,10 @@ def create_app(config_name=None):
 
 def register_extensions(app):
     db.init_app(app)
-
+    bootstrap.init_app(app)
 
 def register_blueprints(app):
-    pass
+    app.register_blueprint(main_bp)
 
 
 def register_errorhandlers(app):
---
+++ awesome_flask_webapp/blueprints/main.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+
+main_bp = Blueprint('main', __name__)
+
+
+@main_bp.route('/')
+def index():
+    return render_template('main/index.html')
+
+
+@main_bp.route('/about')
+def about():
+    return '<h1>about</h1>'
