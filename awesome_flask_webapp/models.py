033e9519bfa4edaf699b48ee40f66da4ab0c03ad
yeh-George <ycj012@163.com>
2020-04-05 20:45:52 +0800

    delete Post.body index owning to some wrong about text index in mysql.

--- awesome_flask_webapp/models.py
+++ awesome_flask_webapp/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import datetime
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import current_app
@@ -237,7 +238,7 @@ class Tag(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), index=True)
-    body = db.Column(db.Text, index=True)
+    body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     can_comment = db.Column(db.Boolean, default=True)
 
